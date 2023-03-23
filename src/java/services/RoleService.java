d0888ec33ef841bb03a4d47143425a47a51eecb2
Angelo Hernandez <122906206+AngeloHernandezSAIT@users.noreply.github.com>
2023-03-23 11:13:09 -0600

    Add files via upload

---
+++ src/java/services/RoleService.java
@@ -0,0 +1,19 @@
+package services;
+
+import dataaccess.RoleDB;
+import java.util.List;
+import models.Role;
+
+public class RoleService {
+    public Role get(int roleID) throws Exception {
+        RoleDB roleDB = new RoleDB();
+        Role role = roleDB.getRole(roleID);
+        return role;
+    }
+    
+    public List<Role> getAll() throws Exception {
+        RoleDB roleDB = new RoleDB();
+        List<Role> roles = roleDB.getAllRole();
+        return roles;
+    }
+}
